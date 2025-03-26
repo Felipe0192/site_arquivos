d5e8b7443fecc14654e9df7c2346590a660683ee
Felipe Oliveira <felipe.camargofilho@gmail.com>
2025-03-26 15:43:56 -0300

    salvei os valores da entrada do buttom

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function valor_teste(event) {
+    console.log(event.target.value);
+}
+
+// agora esta printando no console o caminho do arquivo
+//  mt facil ter feito esse place holder e dps passado pro original
+
+ //puxou o codigo sem o valuekkkkk
+//acho que nao tava indo pq o js tava careegando mt cedo 
+// pq ele ta la em cima no body, o ocrreto é deixar o link do scrpt la no final do body
