8cc5d751955b402dbcffef7e04ddd41f4aebd6f3
Felipe Oliveira <felipe.camargofilho@gmail.com>
2025-03-26 19:41:02 -0300

    coloquei o display de forma interativa

--- scripts.js
+++ scripts.js
@@ -1,6 +1,49 @@
+var arquivo_original;// path completo do arquivo original
+ // caminho encurtado do path do arquivo original para ficar melhor de ver
+
+
+
 function valor_teste(event) {
-    console.log(event.target.value);
+    arquivo_original = event.target.files[0].name // isso teoricamente vai puxar so o nome do arquivo nao o path q nao vai importar para o back pq ele vai mandar o arquivo real
+    console.log(arquivo_original);
+
+    if (arquivo_original !== null) {    //tem que ficar ai pra continuar dps chama outra funçao para resolver isso do display
+        display_converter();
+        coloca_arquivo_original();
+        
+
+    } 
 }
+
+function display_converter(){
+
+    document.getElementById("div-button").style.display = "none";
+    document.getElementById("conversor").style.display = "block"; //esse block ele tipo coloca
+                                                                //  n sei pq acho que adiciona o bloco do display
+
+}
+
+
+//agora vou colocar o path do arquivo original
+//  mudando o nome da dive usando o classname e usa o query selector para selecionar uma div
+// , mas isso parece que troca o nome da div sei la
+
+
+function coloca_arquivo_original(){
+    
+    document.getElementById("arquivo-original").innerHTML = arquivo_original;
+
+}
+ //o inner ele escreve ou edita um texto dentro do html
+
+
+
+
+
+
+// no js o if tem que estar entre parenteses
+
+
 
 // agora esta printando no console o caminho do arquivo
 //  mt facil ter feito esse place holder e dps passado pro original
